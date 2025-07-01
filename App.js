020a93baa986033e9096dd7e84647ff7d091045f
surendra-19 <surendrachagantipati@gmail.com>
2025-07-02 00:09:11 +0530

    added parcel

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const App = React.createElement("h1",{},"Hello");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
